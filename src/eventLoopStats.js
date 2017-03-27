33e4f326eb5c0a99d5c08d6f853dd2f473ceb718
Dávid Szakállas <david.szakallas@gmail.com>
2017-03-27 16:03:33 +0200

    publish binaries to s3

--- src/eventLoopStats.js
+++ src/eventLoopStats.js
@@ -1,2 +1,6 @@
-var eventLoopStats = require('../build/Release/eventLoopStats');
+var binary = require('node-pre-gyp');
+var path = require('path');
+var bindingPath = binary.find(path.resolve(path.join(__dirname, '..', 'package.json')));
+var eventLoopStats = require(bindingPath);
+
 exports.sense = eventLoopStats.sense;
